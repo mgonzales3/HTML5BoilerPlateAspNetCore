a27395ac192aceccc31478955b4561d1634ec8ff
manny gonzales <manny.y.gonzales@gmail.com>
2019-01-03 21:19:54 -0600

    initial

---
+++ BoilerPlateAspNetCore/BoilerPlateAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BoilerPlateAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
